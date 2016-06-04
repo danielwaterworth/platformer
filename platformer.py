beb131423b6730046809505c2ceb0bf2b2b3b9f8
Daniel Waterworth <da.waterworth@gmail.com>
2016-06-04 13:45:42 +0100

    jumping

--- platformer.py
+++ platformer.py
@@ -2,6 +2,7 @@ import sys, pygame
 import itertools
 
 front_file = './p1_walk/p1_front.png'
+jump_file = './p1_walk/p1_jump.png'
 
 walk_files = [
     './p1_walk/p1_walk01.png',
@@ -25,25 +26,30 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 
 front_image = pygame.image.load(front_file)
+jump_image = pygame.image.load(jump_file)
 right_walk_images = [pygame.image.load(filename) for filename in walk_files]
 left_walk_images = [pygame.transform.flip(image, True, False) for image in right_walk_images]
 
-def right_walk_iterator():
+def right_player_iterator():
     return itertools.cycle(right_walk_images)
 
-def left_walk_iterator():
+def left_player_iterator():
     return itertools.cycle(left_walk_images)
 
 stationary_iterator = itertools.repeat(front_image)
-walk_iterator = stationary_iterator
+jump_iterator = itertools.repeat(jump_image)
+player_iterator = stationary_iterator
 
-image = next(walk_iterator)
+image = next(player_iterator)
 x = 0
+y = 0
+y_vect = 0
 
 pygame.time.set_timer(pygame.USEREVENT, 16)
 
 travelling_left = False
 travelling_right = False
+in_air = False
 
 while True:
     for event in pygame.event.get():
@@ -54,32 +60,49 @@ while True:
 
             left_pressed = keys[pygame.K_LEFT] == 1
             right_pressed = keys[pygame.K_RIGHT] == 1
+            up_pressed = keys[pygame.K_UP] == 1
+            down_pressed = keys[pygame.K_DOWN] == 1
 
             go_left = left_pressed and not right_pressed
             go_right = right_pressed and not left_pressed
+            go_up = up_pressed and not down_pressed
 
-            if go_left and travelling_left:
-                x -= 5
-            elif go_right and travelling_right:
-                x += 5
-            else:
+            if go_up and not in_air:
+                in_air = True
                 travelling_left = False
                 travelling_right = False
+                player_iterator = jump_iterator
+                y_vect = 15
 
-                if go_left:
-                    travelling_left = True
-                    walk_iterator = left_walk_iterator()
-                elif go_right:
-                    travelling_right = True
-                    walk_iterator = right_walk_iterator()
+            if in_air:
+                y += y_vect
+                y_vect -= 1
+                if y < 0:
+                    y = 0
+                    in_air = False
+            else:
+                if go_left and travelling_left:
+                    x -= 5
+                elif go_right and travelling_right:
+                    x += 5
                 else:
-                    walk_iterator = stationary_iterator
+                    travelling_left = False
+                    travelling_right = False
 
-            image = next(walk_iterator)
+                    if go_left:
+                        travelling_left = True
+                        player_iterator = left_player_iterator()
+                    elif go_right:
+                        travelling_right = True
+                        player_iterator = right_player_iterator()
+                    else:
+                        player_iterator = stationary_iterator
+
+            image = next(player_iterator)
 
     screen.fill(black)
     rect = image.get_rect()
-    rect.bottom = 600
+    rect.bottom = 600 - y
     rect.x = x
     screen.blit(image, rect)
     pygame.display.flip()
